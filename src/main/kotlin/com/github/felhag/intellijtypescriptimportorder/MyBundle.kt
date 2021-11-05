128debd9f469a95dc44a76dd048b16c687cc73e0
GitHub Action <action@github.com>
2021-11-05 13:23:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/felhag/intellijtypescriptimportorder/MyBundle.kt
+++ src/main/kotlin/com/github/felhag/intellijtypescriptimportorder/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.felhag.intellijtypescriptimportorder
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/felhag/intellijtypescriptimportorder/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.felhag.intellijtypescriptimportorder.services
+
+import com.github.felhag.intellijtypescriptimportorder.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
